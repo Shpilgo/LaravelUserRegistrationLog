5b01bff393df1e18c22b810f10f8605cfc4785f5
Shpilgo <shpilgo@gmail.com>
2020-11-06 18:03:56 +0200

    #3 UserRegistrationLog added

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
+use App\Models\UserRegistrationLog;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -25,10 +26,32 @@ class CreateNewUser implements CreatesNewUsers
             'password' => $this->passwordRules(),
         ])->validate();
 
-        return User::create([
+        $user_created = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+
+        $this->add_to_log($user_created->getAttributes());
+
+        return $user_created;
     }
+
+    private function add_to_log(array $user_attributes)
+    {
+        if (gettype($user_attributes) === 'array'
+            && isset($user_attributes['id'])
+            && is_integer($user_attributes['id'])
+            && isset($user_attributes['created_at'])
+            && is_string($user_attributes['created_at'])
+        ) {
+            $log_record = new UserRegistrationLog;
+
+            $log_record->user_id = $user_attributes['id'];
+            $log_record->user_registration_date_time = $user_attributes['created_at'];
+
+            $log_record->save();
+        }
+    }
+
 }
